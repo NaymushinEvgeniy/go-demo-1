cd76b5524fdbd2d87834c05266df2f9402a2f1e4
Evgeniy MacMini <evgeniy@naymushin.xyz>
2024-01-22 00:57:26 +0300

    Cont learn

---
+++ chapter2/pass_fail.go
@@ -0,0 +1,19 @@
+// Сообщает, сдал ли пользователь экзамен
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(input)
+}
